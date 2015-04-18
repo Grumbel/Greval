f8d41bc33186f61124e18e24874b777aa11feeb1
Ingo Ruhnke <grumbel@gmail.com>
2015-04-18 15:56:40 +0200

    Added empty placeholder testcase

---
+++ test/expr_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "eval_visitor.hpp"
+#include "print_visitor.hpp"
+#include "lexer.hpp"
+#include "parser.hpp"
+#include "syntax_tree.hpp"
+
+TEST(ExprTest, test)
+{
+}
+
+/* EOF */
